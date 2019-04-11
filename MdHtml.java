727b857ab09a459452a03b93a6dd751cf3d4688a
Samuel Pocino <43758828+spocino@users.noreply.github.com>
2019-04-11 14:53:16 -0400

    Some more boilerplate
    imported IO and scanner and main function

--- MdHtml.java
+++ MdHtml.java
@@ -1,5 +1,17 @@
 /* Samuel Pocino
    Java: Markdown to HTML
-   Created 4/10/2019 
+   Created 4/10/2019
+   
+   Java-MD2HTML reads the arguments, which should be relative locations of *.md or *.txt files and 
+   convert markdown syntax to HTML syntax. If no arguments are given, Java-MD2HTML converts markdown
+   from System.in.
 */
 import java.util.Scanner;
+import java.io.*;
+
+public class MdHtml{
+
+   public static void main(String args[]) throws IOException {
+     
+   }
+}
